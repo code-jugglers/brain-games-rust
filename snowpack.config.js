485377a5c82bb4f797a6a83aa845dfafd3721cf2
dblue <dblue@deloitte.com>
2021-04-06 11:56:09 -0400

    optimize

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,11 +2,6 @@ module.exports = {
   workspaceRoot: false,
   mount: {
     www: '/',
-    pkg: {
-      url: '/brain_games',
-      static: true,
-      resolve: false,
-    },
   },
   optimize: {
     minify: true,
